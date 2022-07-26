a2e6c11ecd566e6ea7830d3dca79239d38daf86f
csmelek <103779218+csmelek@users.noreply.github.com>
2022-07-26 15:08:50 +0300

    Logic Layer And UI

---
+++ DALLayer/Baglanti.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DALLayer
+{
+    public class Baglanti
+    {
+       public static  SqlConnection bgl = new SqlConnection(@"Data Source=DESKTOP-D0LP1EG;Initial Catalog=DBPersonel;Integrated Security=True");
+
+    }
+}
---
+++ LogicLayer/LogicPersonel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer;
+using DALLayer;
+
+namespace LogicLayer
+{
+    public class Class1
+    {
+        public static List<EntityPersonel>LLPersonelListesi()
+        {
+            return DALPersonel.PersonelListesi();
+        }
+
+    }
+}
